0fb93d98d992872bc2b2f40b5e01874a46aabe9b
EmreDemirelsLab <h.emredemirel@gmail.com>
2025-03-03 20:52:19 +0300

    Yeni

---
+++ Intro_01/03_Lab.py
@@ -0,0 +1,21 @@
+
+
+# Loops
+# Tekrarlı işlemler yaptıracağımız zaman döngüleri kullanıyoruz. Örneğin kullanıcıdan 10 tane input aldırmak istersek,
+  #ya da 0-9 yani rakamları ekrana yazdırmak istersek bir string ifadenin içerisinde karakter karakter onları kullanmak
+  #istersek loop kullanıyoruz.
+
+# While & For Loop türleri.
+
+# While
+counter = 0
+while counter < 10:
+    print(counter)
+    counter = counter + 1
+
+# 0 - 100 arası her bir tam sayısı ekrana yazdırın.
+
+counter = 0
+while counter < 101:
+    print(counter)
+    counter = counter +1
