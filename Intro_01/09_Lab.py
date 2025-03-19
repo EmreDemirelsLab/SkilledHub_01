c81a82ff3c8c9c4161bf1cf4e7d59f80e7f869a5
EmreDemirelsLab <h.emredemirel@gmail.com>
2025-03-19 19:44:46 +0300

    SkilledHub

--- Intro_01/09_Lab.py
+++ Intro_01/09_Lab.py
@@ -1,4 +1,4 @@
-
+import string
 
 # # Custom Function
 #
@@ -49,44 +49,94 @@
 
 # sign up - sign in
 
-users = [
-    ('beast', '123'),
-    ('savage', '987'),
-    ('bear', '567')
-]
-
-def sign_in(username: str, password: str) -> str:
-    for userName, pwd in users:
-        if userName == username and pwd == password:
-            return f'{username} Hoş Geldiniz.'
-
-    return 'Bilgiler Hatalı..!'
-
-print('Giriş Yapmak İçin Bilgilerinizi Giriniz..!')
-result = sign_in(input('Username: '), input('Password:'))
-print(result)
-
-
-def sign_up(username: str, password: str) -> str:
-    for userName, pwd in users:
-        if userName == username: # in, enumurate  dene
-            return 'Bu Kullanıcı Zaten Alındı..!'
-    users.append((username, password))
-    return 'Üyelik İşlemi Tamamlandı.'
-
-print('Lütfen Yeni Kullanıcı Bilgilerinizi Giriniz..!')
-username = input('Username: ')
-password = input('Password: ')
-
-result = sign_up(username, password)
-print(result)
-print(users)
-
-# yeni kullanıcı giriş yapmayı deniyor
-login_result = sign_in(username, password)
-print(login_result)
+# users = [
+#     ('beast', '123'),
+#     ('savage', '987'),
+#     ('bear', '567')
+# ]
+# def sign_in(username: str, password: str) -> str:
+#         for userName, pwd in users:
+#             if userName == username and pwd == password:
+#                 return f'{username} Hoş Geldiniz.'
+#
+#         return 'Bilgiler Hatalı..!'
+#
+# print('Giriş Yapmak İçin Bilgilerinizi Giriniz..!')
+# while True:
+#     result = sign_in(input('Username: '), input('Password:'))
+#     print(result)
+#     if result != 'Bilgiler Hatalı..!':
+#         break
+#
+#
+# def sign_up(username: str, password: str) -> str:
+#     for userName, pwd in users: #for user in users:   ---   if user[0].__eq__(username):  --   return 'bu kullanıcı alındı'
+#         if userName == username: # in, enumurate  dene
+#             return 'Bu Kullanıcı Zaten Alındı..!'
+#     pwd_is_valid = is_pwd_valid(password=password)
+#     if pwd_is_valid:
+#
+#         users.append((username, password))
+#         return 'Üyelik İşlemi Tamamlandı.'
+#     return 'Invalid password'
+#
+#
+# def is_pwd_valid(password: str) -> bool:
+#     char_set = set(password)
+#
+#     is_result = (
+#         len(password) >= 16
+#         and any(c in string.ascii_uppercase for c in char_set)
+#         and any(c in string.ascii_lowercase for c in char_set)
+#         and any(c in string.digits for c in char_set)
+#         and any(c in string.punctuation for c in char_set)
+#     )
+#
+#     return is_result
+# print('Lütfen Yeni Kullanıcı Bilgilerinizi Giriniz..!')
+# while True:
+#     username = input('Username: ')
+#     password = input('Password: ')
+#
+#     result = sign_up(username, password)
+#     print(result)
+#     print(users)
+#
+# # yeni kullanıcı giriş yapmayı deniyor
+# login_result = sign_in(username, password)
+# print(login_result)
 
 # 1. Yukarıda ki problemi çözün kullanıcı adı tek olacak uniq
 # 2. kişi sign up olunca hemen feedback verilsin 'üyelik işlemi tamamlandı.' akabinde sign_in fonksiyonunu tetiklesin.
 # 3. bu kodlara fonksiyonlaştırdığımız is_valid_ entegre et
 # 4. Okuma ödevi SoC (Seperation of Concern) & SRP (Signle Responsibility Principle)
+
+
+# hash generator yap
+
+# sıfırdan proje açılacak
+# açılan projenin adı Software_Principle
+# SOC.py
+# SRC.py
+# DRY.py
+
+# aşağıdaki listede tekrar eden sayıları saptayalım ve aşağıdaki sözlük formatında ekrana yazdıralım.
+# frequency_dict = {
+#     1: 4
+#     2: 3
+#     3: 2
+# }
+
+
+rakamlar = [1, 1, 1, 5, 3, 3, 5, 2, 4, 1, 5, 5, 2, 2]
+
+def frequency_count(number: list) -> dict:
+    frequency_dict = {}
+    for number in rakamlar:
+        if number in frequency_dict:
+            frequency_dict[number] += 1
+        else:
+            frequency_dict[number] = 1
+    return frequency_dict
+
+print(frequency_count(numbers=rakamlar))
